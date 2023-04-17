363c8922c9aaa4cde645e6ca77775abf72b6702b
youssef-ASSOIL <assoilyoussef02@gmail.com>
2023-04-17 03:12:53 +0000

    with Menu00

--- flutter_application_1/lib/screens/home_Screen_Chef_Project.dart
+++ flutter_application_1/lib/screens/home_Screen_Chef_Project.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -9,12 +10,158 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+ @override
   Widget build(BuildContext context) {
-    return Center(
-      
-      child: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Text('Zaid !!',style: GoogleFonts.robotoCondensed(fontSize: 20,fontWeight: FontWeight.bold))
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color.fromARGB(255, 86, 162, 156), Color.fromARGB(255, 156, 237, 187)],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 16, 231, 213),
+                      Color(0xFF3c6e71),
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(36),
+                    bottomRight: Radius.circular(36),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 10),
+                        child: Text(
+                          'SafeDispose',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.robotoCondensed(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: _buildClientCard('images/client.png', 'Clients'),
+                        ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: _buildClientCard('images/business.png', 'Employee'),
+                        ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: _buildClientCard('images/manager.png', 'Managers'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 50),
+                    Text(
+                      'Welcome to SafeDispose',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.robotoCondensed(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.help),
+                      label: Text('Help !!!'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFF019267),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildClientCard(String imagePath, String title) {
+    return Container(
+      height: 150,
+      width: 120,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF019267),
+            Color(0xFF017A58),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 22,
+            color: Color(0xFF227C70).withOpacity(0.5),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            imagePath,
+            height: 50,
+            width: 50,
+          ),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
