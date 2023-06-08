f6d883f77997d675570e1ac026105e8d32bda327
foliogi45 <aminewapo05@gmail.com>
2023-06-08 23:29:10 +0100

    amine

--- flutter_application_1/lib/Auth.dart
+++ flutter_application_1/lib/Auth.dart
@@ -10,17 +10,14 @@ class Auth extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: StreamBuilder<User?>(
-        stream:  FirebaseAuth.instance.authStateChanges(),
-        builder: ((context,snapshot){
-           if (snapshot.hasData) {
-             return HomeScreen();
-                   
-           }else{
-            return login_screen();
-          }
-
-        })
-      ),
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasData) {
+              return const HomeScreen();
+            } else {
+              return const login_screen();
+            }
+          })),
     );
   }
 }
